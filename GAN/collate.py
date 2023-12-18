b06a8ea7d98be25beadac2b5ca67371cb82baf21
Aiza Maksutova <71812108+aizamaksutova@users.noreply.github.com>
2023-12-18 23:20:21 +0300

    Add files via upload

---
+++ GAN/collate.py
@@ -0,0 +1,21 @@
+from typing import List, Tuple
+
+import torch
+
+
+def collate_w_target_fn(batch: List[Tuple]):
+    img = []
+    target = []
+    for i, t in batch:
+        img.append(i)
+        target.append(t)
+    return {
+        "img": torch.stack(img),
+        "target": torch.tensor(target),
+    }
+
+
+def collate_fn(batch: List[Tuple]):
+    return {
+        "img": torch.stack(batch),
+    }

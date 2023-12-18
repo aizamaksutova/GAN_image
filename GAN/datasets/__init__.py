2d19f29b7c5b1ebca060ceacfb0a4fc7bbf3941c
Aiza Maksutova <71812108+aizamaksutova@users.noreply.github.com>
2023-12-18 23:14:33 +0300

    Create __init__.py

---
+++ GAN/datasets/__init__.py
@@ -0,0 +1,7 @@
+from GAN.datasets.artbench10_256x256 import ArtBench10_256x256
+from GAN.datasets.cats_faces import CatsFaces
+from GAN.datasets.base_dataset import InMemoryArtBench10_256x256, CatsBase
+
+
+__all__ = ["ArtBench10_32x32", "ArtBench10_256x256", "CatsFaces", "MNISTWrapper", 
+            "InMemoryArtBench10_256x256", "CatsBase"]
